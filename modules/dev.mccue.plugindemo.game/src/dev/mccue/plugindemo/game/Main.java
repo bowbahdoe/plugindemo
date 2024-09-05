e2f6f4d7206936301acc8ebfc2627b83add75973
Ethan McCue <ethan@mccue.dev>
2024-09-05 14:24:50 -0400

    Put plugins in different folder

--- modules/dev.mccue.plugindemo.game/src/dev/mccue/plugindemo/game/Main.java
+++ modules/dev.mccue.plugindemo.game/src/dev/mccue/plugindemo/game/Main.java
@@ -2,14 +2,46 @@ package dev.mccue.plugindemo.game;
 
 import javax.swing.*;
 import java.awt.*;
+import java.lang.module.ModuleDescriptor;
+import java.lang.module.ModuleFinder;
+import java.lang.module.ModuleReference;
+import java.nio.file.Path;
 import java.util.ServiceLoader;
+import java.util.Set;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import dev.mccue.plugindemo.api.Entity;
 
 public final class Main {
     public static void main(String[] args) {
-        var frame = new JFrame("game");
+        Path pluginPath = null;
+        if (args.length > 0) {
+            pluginPath = Path.of(args[0]);
+        }
 
-        var entities = ServiceLoader.load(Entity.class)
+        ModuleLayer moduleLayer;
+        if (pluginPath == null) {
+            moduleLayer = ModuleLayer.boot();
+        }
+        else {
+            var moduleFinder = ModuleFinder.of(pluginPath);
+            var moduleNames = moduleFinder.findAll()
+                    .stream()
+                    .map(ModuleReference::descriptor)
+                    .map(ModuleDescriptor::name)
+                    .collect(Collectors.toSet());
+
+            var parentLayer = ModuleLayer.boot();
+            var configuration = parentLayer.configuration()
+                    .resolve(moduleFinder, ModuleFinder.of(), moduleNames);
+            var systemClassLoader = ClassLoader.getSystemClassLoader();
+            moduleLayer = parentLayer.defineModulesWithOneLoader(
+                    configuration, systemClassLoader
+            );
+        }
+
+        var entities = ServiceLoader.load(moduleLayer, Entity.class)
                 .stream()
                 .map(ServiceLoader.Provider::get)
                 .toList();
@@ -22,7 +54,6 @@ public final class Main {
                 for (int i = 0; i < entities.size(); i++) {
                     entities.get(i).draw((Graphics2D) g, i * 100, i * 100);
                 }
-
             }
         };
 
@@ -33,6 +64,7 @@ public final class Main {
                 )
         ));
 
+        var frame = new JFrame("game");
         frame.add(panel);
         frame.pack();
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
--- scripts/src/Project.java
+++ scripts/src/Project.java
@@ -1,3 +1,4 @@
+import dev.mccue.tools.jar.Jar;
 import dev.mccue.tools.java.Java;
 import dev.mccue.tools.javac.Javac;
 import picocli.CommandLine;
@@ -6,22 +7,36 @@ import java.nio.file.Files;
 import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
 import java.util.Comparator;
+import java.util.List;
 
 @CommandLine.Command(name = "project")
 public class Project {
     static final Path BUILD = Path.of("build");
+    static final Path PLUGINS = Path.of("plugins");
     static final String MODULE_SOURCE_PATH = "./modules/*/src";
 
     boolean cleaned = false;
     @CommandLine.Command(name = "clean")
     public void clean() throws Exception {
         if (!cleaned && Files.exists(BUILD)) {
-            try (var pathStream = Files.walk(BUILD)) {
-                var paths = pathStream.sorted(Comparator.reverseOrder()).toList();
-                for (var path : paths) {
-                    Files.deleteIfExists(path);
+            if (Files.exists(BUILD)) {
+                try (var pathStream = Files.walk(BUILD)) {
+                    var paths = pathStream.sorted(Comparator.reverseOrder()).toList();
+                    for (var path : paths) {
+                        Files.deleteIfExists(path);
+                    }
                 }
             }
+
+            if (Files.exists(PLUGINS)) {
+                try (var pathStream = Files.walk(PLUGINS)) {
+                    var paths = pathStream.sorted(Comparator.reverseOrder()).toList();
+                    for (var path : paths) {
+                        Files.deleteIfExists(path);
+                    }
+                }
+            }
+
             cleaned = true;
         }
     }
@@ -49,16 +64,52 @@ public class Project {
     }
 
     @CommandLine.Command(
+            name = "package",
+            description = "Packages all modules"
+    )
+    public void package_() throws Exception {
+        compile();
+
+        Jar.run(arguments -> {
+            arguments.__create()
+                    .__file(BUILD.resolve("jar/game/dev.mccue.plugindemo.api.jar"))
+                    ._C(BUILD.resolve("javac/dev.mccue.plugindemo.api"), ".");
+        });
+
+
+        Jar.run(arguments -> {
+            arguments.__create()
+                    .__file(BUILD.resolve("jar/game/dev.mccue.plugindemo.game.jar"))
+                    ._C(BUILD.resolve("javac/dev.mccue.plugindemo.game"), ".");
+        });
+
+
+        Jar.run(arguments -> {
+            arguments.__create()
+                    .__file(PLUGINS.resolve("dev.mccue.plugindemo.square.jar"))
+                    ._C(BUILD.resolve("javac/dev.mccue.plugindemo.square"), ".");
+        });
+
+
+        Jar.run(arguments -> {
+            arguments.__create()
+                    .__file(PLUGINS.resolve("dev.mccue.plugindemo.circle.jar"))
+                    ._C(BUILD.resolve("javac/dev.mccue.plugindemo.circle"), ".");
+        });
+    }
+
+    @CommandLine.Command(
             name = "run",
             description = "Runs the code"
     )
     public void run() throws Exception {
-        compile();
+        package_();
         Java.run(arguments -> {
             arguments
-                    .__module_path(BUILD.resolve("javac"))
+                    .__module_path(BUILD.resolve("jar/game"))
                     .__add_modules("ALL-MODULE-PATH")
-                    .__module("dev.mccue.plugindemo.game/dev.mccue.plugindemo.game.Main");
+                    .__module("dev.mccue.plugindemo.game/dev.mccue.plugindemo.game.Main")
+                    .add("plugins");
         });
     }
 
